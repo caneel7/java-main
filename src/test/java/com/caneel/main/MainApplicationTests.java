fa984629f4c6454c6429fe379020c87dea572f10
caneel7 <119056687+caneel7@users.noreply.github.com>
2024-06-20 07:35:09 -0700

    Init
    First Commit Initialization.

---
+++ src/test/java/com/caneel/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caneel.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
